da03507298259f6d11818c35a65c71b238348e00
webbgroup <joel@webbgroup.com>
2016-04-29 10:12:32 -0400

    Update cloudgear.py

--- cloudgear.py
+++ cloudgear.py
@@ -114,8 +114,8 @@ def initialize_system():
     execute("apt-get autoclean -y" , True)
     execute("apt-get update -y" , True)
     execute("apt-get install ubuntu-cloud-keyring python-setuptools python-iniparse python-psutil -y", True)
-    delete_file("/etc/apt/sources.list.d/havana.list")
-    execute("echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu precise-updates/icehouse main >> /etc/apt/sources.list.d/icehouse.list")
+    delete_file("/etc/apt/sources.list.d/liberty.list")
+    execute("echo deb http://ubuntu-cloud.archive.canonical.com/ubuntu trusty-updates/liberty main >> /etc/apt/sources.list.d/liberty.list")
     # Stupid proxy causing timeouts.
     time.sleep(10)
     execute("apt-get update -y", True)
